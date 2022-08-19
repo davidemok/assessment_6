7a3cbf42a36756d50254e62d28623f71c30bd3aa
David Mok <davidemok@gmail.com>
2022-08-19 11:28:57 -0500

    added trello link, extra credit

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -21,6 +21,12 @@ test('Title shows up when page loads', async () => {
     expect(displayed).toBe(true)
 })
 
+test('see all bots button shows up when page loads', async () => {
+    const seeAllBots = await driver.findElement(By.id('see-all'))
+    const displayed = await seeAllBots.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
 test('clicking Draw button displays div id "choices"', async () => {
     let drawButton = await driver.findElement(By.id('draw'))
     await drawButton.sendKeys('\n')
@@ -29,14 +35,15 @@ test('clicking Draw button displays div id "choices"', async () => {
     expect(newDiv).toBeTruthy();
 })
 
-// test('clicking Add to Duo button displays div id "player-duo"', async () => {
-//     let addButton = await driver.findElement(By.id('addToDuo'))
-//     await addButton.sendKeys('\n')
-//     await driver.sleep(2000)
-//     let newDiv2 = await driver.findElement(By.id('player-duo'))
-//     expect(newDiv2).toBeTruthy();
-// })
-//hmmm this doesn't like my id addToDuo which i thought i added in index.js but i figured it was a longshot lol
+test('clicking Add to Duo button displays div id "player-duo"', async () => {
+    await driver.sleep(3000)
+    let addButton = await driver.findElement(By.className('bot-btn'))
+    await addButton.sendKeys('\n')
+    await driver.sleep(2000)
+    let newDiv2 = await driver.findElement(By.id('player-duo'))
+    expect(newDiv2).toBeTruthy();
+})
+// hmmm i can't figure out how to select the addButton which (i think) is created in index.js
 
 // test('see all bots button works', async () => {
 //     let seeBots = await driver.findElement(By.id('see-all'))
@@ -45,5 +52,7 @@ test('clicking Draw button displays div id "choices"', async () => {
 //     let allBots = await driver.findElement(By.id('all-bots'))
 //     expect(allBots).Contain(10)
 // })
+//i can't figure out how to test if allbots contains divs inside it :(
+
 
 })
--- heroku.md
+++ heroku.md
@@ -1,2 +1,3 @@
 ## Paste your link below
 
+https://duel-duo-assessment6.herokuapp.com/
